5aeccb34a8d8debc89483b657a91c14a3a052b16
asus <asus@DESKTOP-9UKBS01>
2022-04-11 08:59:47 +0700

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class FormWeek7
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dvgPlayer = new System.Windows.Forms.DataGridView();
+            this.btnQuery = new System.Windows.Forms.Button();
+            this.txbQuery = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dvgPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dvgPlayer
+            // 
+            this.dvgPlayer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvgPlayer.Location = new System.Drawing.Point(12, 106);
+            this.dvgPlayer.Name = "dvgPlayer";
+            this.dvgPlayer.Size = new System.Drawing.Size(678, 154);
+            this.dvgPlayer.TabIndex = 0;
+            // 
+            // btnQuery
+            // 
+            this.btnQuery.Location = new System.Drawing.Point(615, 12);
+            this.btnQuery.Name = "btnQuery";
+            this.btnQuery.Size = new System.Drawing.Size(75, 23);
+            this.btnQuery.TabIndex = 1;
+            this.btnQuery.Text = "Query";
+            this.btnQuery.UseVisualStyleBackColor = true;
+            this.btnQuery.Click += new System.EventHandler(this.btnQuery_Click);
+            // 
+            // txbQuery
+            // 
+            this.txbQuery.Location = new System.Drawing.Point(12, 12);
+            this.txbQuery.Name = "txbQuery";
+            this.txbQuery.Size = new System.Drawing.Size(597, 20);
+            this.txbQuery.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 57);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(154, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // FormWeek7
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 272);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txbQuery);
+            this.Controls.Add(this.btnQuery);
+            this.Controls.Add(this.dvgPlayer);
+            this.Name = "FormWeek7";
+            this.Text = "Form Week 7";
+            this.Load += new System.EventHandler(this.FormWeek7_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dvgPlayer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dvgPlayer;
+        private System.Windows.Forms.Button btnQuery;
+        private System.Windows.Forms.TextBox txbQuery;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class FormWeek7 : Form
+    {
+        public FormWeek7()
+        {
+            InitializeComponent();
+            
+        }
+        public static string sqlConnection = "server=localhost;uid=root;pwd=;database=premier_league";
+        public MySqlConnection sqlConnect = new MySqlConnection(sqlConnection);
+        public MySqlCommand sqlCommand;
+        public MySqlDataAdapter sqlAdapter; 
+        public string sqlQuery;
+
+
+        private void FormWeek7_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnQuery_Click(object sender, EventArgs e)
+        {
+            
+            DataTable dtPlayer = new DataTable();
+            //mulai dari sini 
+            sqlQuery = txbQuery.Text;
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(dtPlayer);
+            //sampai disini 
+            dvgPlayer.DataSource = dtPlayer;
+        }
+    }
+}
